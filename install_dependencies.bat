083bd47535c72d171610217075d0ba43bbdee177
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-02-12 18:28:10 +0100

    Batch Files
    Files to make the installation of dependencies and starting up the bot easier (Only aviable for windows)

---
+++ install_dependencies.bat
@@ -0,0 +1,4 @@
+@echo "Installing dependencies..."
+npm i
+@echo "Installation completed, Open start.bat to start your bot"
+PAUSE
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo "Starting your discord bot!"
+node index.js
+@echo "Your bot is now online!"
+PAUSE
